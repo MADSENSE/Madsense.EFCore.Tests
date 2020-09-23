31ee752d6c3c1578f843793a252a10ab251ee756
Yann Zahringer Ferrando <yann.zahringer@madsense.com>
2020-09-23 08:38:28 +0200

    encrypted db tests added

--- Madsense.EFCore3.Tests/DbContextTests.cs
+++ Madsense.EFCore3.Tests/DbContextTests.cs
@@ -11,16 +11,32 @@ namespace Madsense.EFCore.Tests
     public class DbContextTests
     {
         [Theory]
-        [InlineData(ServiceLifetime.Singleton, false, 1, 100)]
-        [InlineData(ServiceLifetime.Transient, false, 3, 100)]
-        [InlineData(ServiceLifetime.Singleton, false, 3, 100)] // SQLite Error 1: 'cannot rollback - no transaction is active'.
-        [InlineData(ServiceLifetime.Transient, true, 3, 100)]
-        [InlineData(ServiceLifetime.Singleton, true, 3, 100)] // SQLite Error 1: 'cannot start a transaction within a transaction'.
-        public async Task Sqlite_ConcurrentSave_Test(ServiceLifetime optionsLifeTime, bool openConnection, int concurrentSaveCount, int insertOperationsCount)
+        [InlineData(ServiceLifetime.Singleton, false, false, 1, 10)]
+        [InlineData(ServiceLifetime.Singleton, false, false, 2, 10)] // SafeHandle cannot be null. (Parameter 'pHandle')
+        [InlineData(ServiceLifetime.Singleton, false, true, 1, 10)]
+        [InlineData(ServiceLifetime.Singleton, false, true, 2, 10)]
+        [InlineData(ServiceLifetime.Singleton, true, false, 1, 10)]
+        [InlineData(ServiceLifetime.Singleton, true, false, 2, 10)] // Invalid attempt to call GetInt32 when reader is closed.
+        [InlineData(ServiceLifetime.Singleton, true, true, 1, 10)]
+        [InlineData(ServiceLifetime.Singleton, true, true, 2, 10)]
+        [InlineData(ServiceLifetime.Transient, false, false, 1, 10)]
+        [InlineData(ServiceLifetime.Transient, false, false, 2, 10)]
+        [InlineData(ServiceLifetime.Transient, false, true, 1, 10)]
+        [InlineData(ServiceLifetime.Transient, false, true, 2, 10)]
+        [InlineData(ServiceLifetime.Transient, true, false, 1, 10)]
+        [InlineData(ServiceLifetime.Transient, true, false, 2, 10)]
+        [InlineData(ServiceLifetime.Transient, true, true, 1, 10)]
+        [InlineData(ServiceLifetime.Transient, true, true, 2, 10)]
+        public async Task Sqlite_ConcurrentSave_Test(ServiceLifetime optionsLifeTime, bool encrypted, bool openConnection, int concurrentSaveCount, int insertOperationsCount)
         {
             // Prepare
             var serviceCollection = new ServiceCollection();
-            var connectionString = $"Filename={Guid.NewGuid()}.db";
+
+            var connectionString = new SqliteConnectionStringBuilder
+            {
+                DataSource = $"Filename={Guid.NewGuid()}.db",
+                Password = encrypted ? "12345" : null
+            }.ToString();
 
             serviceCollection.AddDbContext<AppContext>((s, builder) =>
             {
@@ -60,16 +76,32 @@ namespace Madsense.EFCore.Tests
         }
 
         [Theory]
-        [InlineData(ServiceLifetime.Singleton, false, 1, 100)]
-        [InlineData(ServiceLifetime.Transient, false, 3, 100)]
-        [InlineData(ServiceLifetime.Singleton, false, 3, 100)] // SafeHandle cannot be null. (Parameter 'pHandle')
-        [InlineData(ServiceLifetime.Transient, true, 3, 100)]
-        [InlineData(ServiceLifetime.Singleton, true, 3, 100)]
-        public async Task Sqlite_ConcurrentRead_Test(ServiceLifetime optionsLifeTime, bool openConnection, int concurrentReadCount, int readOperationsCount)
+        [InlineData(ServiceLifetime.Singleton, false, false, 1, 10)]
+        [InlineData(ServiceLifetime.Singleton, false, false, 2, 10)] // Object reference not set to an instance of an object.
+        [InlineData(ServiceLifetime.Singleton, false, true, 1, 10)]
+        [InlineData(ServiceLifetime.Singleton, false, true, 2, 10)] // SQLite Error 1: 'cannot start a transaction within a transaction'.
+        [InlineData(ServiceLifetime.Singleton, true, false, 1, 10)]
+        [InlineData(ServiceLifetime.Singleton, true, false, 2, 10)] // BeginTransaction can only be called when the connection is open.
+        [InlineData(ServiceLifetime.Singleton, true, true, 1, 10)]
+        [InlineData(ServiceLifetime.Singleton, true, true, 2, 10)] // SQLite Error 1: 'cannot start a transaction within a transaction'.
+        [InlineData(ServiceLifetime.Transient, false, false, 1, 10)]
+        [InlineData(ServiceLifetime.Transient, false, false, 2, 10)]
+        [InlineData(ServiceLifetime.Transient, false, true, 1, 10)]
+        [InlineData(ServiceLifetime.Transient, false, true, 2, 10)]
+        [InlineData(ServiceLifetime.Transient, true, false, 1, 10)]
+        [InlineData(ServiceLifetime.Transient, true, false, 2, 10)]
+        [InlineData(ServiceLifetime.Transient, true, true, 1, 10)]
+        [InlineData(ServiceLifetime.Transient, true, true, 2, 10)]
+        public async Task Sqlite_ConcurrentRead_Test(ServiceLifetime optionsLifeTime, bool encrypted, bool openConnection, int concurrentReadCount, int readOperationsCount)
         {
             // Prepare
             var serviceCollection = new ServiceCollection();
-            var connectionString = $"Filename={Guid.NewGuid()}.db";
+
+            var connectionString = new SqliteConnectionStringBuilder
+            {
+                DataSource = $"Filename={Guid.NewGuid()}.db",
+                Password = encrypted ? "12345" : null
+            }.ToString();
 
             serviceCollection.AddDbContext<AppContext>((s, builder) =>
             {
